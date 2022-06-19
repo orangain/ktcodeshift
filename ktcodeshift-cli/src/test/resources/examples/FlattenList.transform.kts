0a23975d97593452b503be84c6829d68ab7b1bba
orangain <orangain@gmail.com>
2022-06-19 15:44:09 +0900

    Move kastree to ktast 0.6.0
    Test fails with "changedSource does not match outputSource."

--- ktcodeshift-cli/src/test/resources/examples/FlattenList.transform.kts
+++ ktcodeshift-cli/src/test/resources/examples/FlattenList.transform.kts
@@ -1,15 +1,15 @@
-import kastree.ast.Node
+import ktast.ast.Node
 import ktcodeshift.*
 
 transform { fileInfo ->
     Api
         .parse(fileInfo.source)
-        .preVisit { v, _ ->
-            if (v is Node.Expr.Call && isListOf(v)) {
+        .preVisit { v, p ->
+            if (v is Node.ValueArgs && p is Node.Expr.Call && isListOf(p)) {
                 v.copy(args = v.args.flatMap { arg ->
                     val expr = arg.expr
                     if (expr is Node.Expr.Call && isListOf(expr)) {
-                        expr.args
+                        expr.args?.args ?: listOf()
                     } else {
                         listOf(arg)
                     }
