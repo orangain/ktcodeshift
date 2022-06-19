792cd9c84e2376bd709cf3d9503a8ca98f9a4716
orangain <orangain@gmail.com>
2022-06-19 23:43:09 +0900

    example: Transform JUnit4 imports

--- ktcodeshift-cli/src/test/resources/examples/JUnit4To5.transform.kts
+++ ktcodeshift-cli/src/test/resources/examples/JUnit4To5.transform.kts
@@ -1,18 +1,35 @@
 import ktast.ast.Node
 import ktcodeshift.*
 
+val annotationNameMap = mapOf(
+    "Before" to "BeforeEach",
+    "After" to "AfterEach",
+    "BeforeClass" to "BeforeAll",
+    "AfterClass" to "AfterAll",
+)
+
 transform { fileInfo ->
     Api
         .parse(fileInfo.source)
         .preVisit { v, _ ->
+            if (v is Node.Import) {
+                if (v.names.size == 3 && v.names.take(2).map { it.name } == listOf("org", "junit")) {
+                    v.copy(names = v.names.take(2) + listOf(
+                            Node.Expr.Name("jupiter"),
+                            Node.Expr.Name("api"),
+                            Node.Expr.Name(v.names[2].name.let { annotationNameMap[it] ?: it }),
+                        )
+                    )
+                } else {
+                    v
+                }
+            } else {
+                v
+            }
+        }
+        .preVisit { v, _ ->
             if (v is Node.Modifier.AnnotationSet.Annotation) {
-                val name = when (v.constructorCallee.type.pieces.last().name.name) {
-                    "Before" -> "BeforeEach"
-                    "After" -> "AfterEach"
-                    "BeforeClass" -> "BeforeAll"
-                    "AfterClass" -> "AfterAll"
-                    else -> null
-                }?.let(Node.Expr::Name)
+                val name = annotationNameMap[v.constructorCallee.type.pieces.last().name.name]?.let(Node.Expr::Name)
 
                 if (name != null) {
                     v.copy(
