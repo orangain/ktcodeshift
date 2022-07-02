b44b94b838dcb9e35c47f722868ef364d9a9858c
orangain <orangain@gmail.com>
2022-07-02 17:52:25 +0900

    Introduce Node.Type.asXXX extension methods

--- ktcodeshift-dsl/src/main/kotlin/ktcodeshift/NodeExtension.kt
+++ ktcodeshift-dsl/src/main/kotlin/ktcodeshift/NodeExtension.kt
@@ -13,3 +13,16 @@ val Node.WithModifiers.literalModifiers: List<Node.Modifier.Lit>
 
 val Node.Decl.Structured.isDataClass: Boolean
     get() = literalModifiers.contains(Node.Modifier.Lit(Node.Modifier.Keyword.DATA))
+
+
+fun Node.Type.asFunctionType(): Node.Type.Func = this as Node.Type.Func
+fun Node.Type.asFunctionTypeOrNull(): Node.Type.Func? = this as? Node.Type.Func
+fun Node.Type.asSimpleType(): Node.Type.Simple = this as Node.Type.Simple
+fun Node.Type.asSimpleTypeOrNull(): Node.Type.Simple? = this as? Node.Type.Simple
+fun Node.Type.asNullableType(): Node.Type.Nullable = this as Node.Type.Nullable
+fun Node.Type.asNullableTypeOrNull(): Node.Type.Nullable? = this as? Node.Type.Nullable
+fun Node.Type.asDynamicType(): Node.Type.Dynamic = this as Node.Type.Dynamic
+fun Node.Type.asDynamicTypeOrNull(): Node.Type.Dynamic? = this as? Node.Type.Dynamic
+
+val Node.TypeArg.type: Node.Type?
+    get() = (this as? Node.TypeArg.Type)?.typeRef?.type
