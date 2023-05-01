08a5ad1d6446c6a123f6a4a8d2bd85bf8607e051
orangain <orangain@gmail.com>
2023-05-01 16:06:08 +0900

    Set languageVersion to 11

--- ktcodeshift-cli/build.gradle.kts
+++ ktcodeshift-cli/build.gradle.kts
@@ -34,7 +34,7 @@ tasks.withType<KotlinCompile> {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
