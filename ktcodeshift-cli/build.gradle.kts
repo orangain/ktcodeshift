32ae1e8754e625c9023386cac88db84d79ed75a2
orangain <orangain@gmail.com>
2023-06-19 22:47:29 +0900

    Use ktast 0.9.0

--- ktcodeshift-cli/build.gradle.kts
+++ ktcodeshift-cli/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("info.picocli:picocli:4.7.3")
     implementation(project(":ktcodeshift-dsl")) // the script definition module
     implementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("com.github.orangain.ktast:ast-psi:main-SNAPSHOT")
+    testImplementation("com.github.orangain.ktast:ast-psi:0.9.0")
 }
 
 tasks.withType<KotlinCompile> {
