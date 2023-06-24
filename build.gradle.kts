77e00f53b567d355641bba949db4b5f497227983
orangain <orangain@gmail.com>
2023-06-24 15:15:59 +0900

    Specify version in all projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     kotlin("jvm") version "1.8.22"
     id("org.jetbrains.dokka") version "1.8.20"
+    // Add support for generating version number from Git status.
+    id("com.palantir.git-version") version "3.0.0"
 }
 
+val gitVersion: groovy.lang.Closure<String> by extra
+
 allprojects {
+    version = gitVersion()
+
     repositories {
         // Use Maven Central for resolving dependencies.
         mavenCentral()
